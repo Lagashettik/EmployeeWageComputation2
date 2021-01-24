05578c6d20633af2cfb5e1c0a596f794b251b54b
Lagashettik <Lagashettik@gmail.com>
2021-01-24 14:53:38 +0530

    Added: Employee Part Time Wage

--- EmployeeWageBuilder2.java
+++ EmployeeWageBuilder2.java
@@ -9,14 +9,19 @@ public class EmployeeWageBuilder2 {
 	void EmployeeWage(){
 
 		int isFullTime=1;
+		int isPartTime=2;
 		int WagePerHour=20;
 		int NoOfHours;
 
-		double empCheck= Math.floor(Math.random() * 10) % 2;
+		double empCheck= Math.floor(Math.random() * 10) % 3;
 
 		if(empCheck == isFullTime) {
 			System.out.println("Employee is Present");
 			NoOfHours=8;
+			}
+		else if (empCheck == isPartTime){
+			System.out.println("Employee is Part Time");
+			NoOfHours=4;
 			}
 		else {
 			System.out.println("Employee is absent");
@@ -25,6 +30,7 @@ public class EmployeeWageBuilder2 {
 
 		int Payment= WagePerHour * NoOfHours;
 		System.out.println("Payment is "+Payment);
+
 		}
 
 	public static void main(String args[]) {
