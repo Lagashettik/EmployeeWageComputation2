6188aa49fd6fa60e69c6fd3bde8351ac7b1681f2
Lagashettik <Lagashettik@gmail.com>
2021-01-24 14:20:01 +0530

    Added: Calculation of Employee Daily Wage

--- EmployeeWageBuilder2.java
+++ EmployeeWageBuilder2.java
@@ -9,18 +9,27 @@ public class EmployeeWageBuilder2 {
 	void EmployeeWage(){
 
 		int isFullTime=1;
+		int WagePerHour=20;
+		int NoOfHours;
 
 		double empCheck= Math.floor(Math.random() * 10) % 2;
 
-		if(empCheck == isFullTime)
+		if(empCheck == isFullTime) {
 			System.out.println("Employee is Present");
-		else
+			NoOfHours=8;
+			}
+		else {
 			System.out.println("Employee is absent");
+			NoOfHours=0;
+			}
 
+		int Payment= WagePerHour * NoOfHours;
+		System.out.println("Payment is "+Payment);
 		}
 
 	public static void main(String args[]) {
 		EmployeeWageBuilder2 employee = new EmployeeWageBuilder2();
+
 		employee.EmployeeWage();
 
 		}
