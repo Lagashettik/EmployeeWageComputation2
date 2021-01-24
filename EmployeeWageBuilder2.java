17238f650fc464740ca381b1a4572eeba2b4ac03
Lagashettik <Lagashettik@gmail.com>
2021-01-24 15:18:11 +0530

    Calculated: Employee Wages till Max allowed Working Hours or Days in Month

--- EmployeeWageBuilder2.java
+++ EmployeeWageBuilder2.java
@@ -7,31 +7,35 @@ public class EmployeeWageBuilder2 {
 		}
 
 	void EmployeeWage(){
-		int isFullTime=1;
-		int isPartTime=2;
 		int WagePerHour=20;
 		int NoOfHours=0;
-		int Month=20;	//Here One Month as 20 Days
+		int TotalHours=100;	// Max working Hours is 100 in One Month
+		int TotalDays=20;	// Max Days in Month is 20 Days
+		int day=1;
 
-		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+		while( day<=TotalDays ){
+			int empCheck=(int) Math.floor(Math.random() * 10) % 2;
+				//empCheck=0 then Employee is Part Time
+				//empCheck=1 then Employee is Full Time
 
-		switch (empCheck) {
-		case 1:
-			System.out.println("Employee is Full Time");
-			NoOfHours=8;
-			break;
-		case 2:
-			System.out.println("Employee is Part Time");
-			NoOfHours=4;
-			break;
-		case 0:
-			System.out.println("Employee is absent");
-			NoOfHours=0;
-			break;
-			}
+			switch (empCheck) {
+				case 1:
+					NoOfHours+=8;
+					break;
+				case 0:
+					NoOfHours+=4;
+					break;
+				}
 
-		int Payment= WagePerHour * NoOfHours * Month;
-		System.out.println("Payment of One Month is "+Payment);
+		if( TotalHours <= NoOfHours ){
+			break;}
+
+		day++;
+	}
+
+		int Payment= WagePerHour * NoOfHours * day;
+		System.out.println("Total Payment of Employee is "+Payment);
+
 		}
 
 	public static void main(String args[]) {
