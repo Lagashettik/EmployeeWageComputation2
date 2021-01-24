7f897e08747e8c68095eedfc688e48d53d275509
Lagashettik <Lagashettik@gmail.com>
2021-01-24 14:09:33 +0530

    Added: Employee Attendance

--- EmployeeWageBuilder2.java
+++ EmployeeWageBuilder2.java
@@ -1,12 +1,27 @@
 
 public class EmployeeWageBuilder2 {
+
+	EmployeeWageBuilder2() {
+		System.out.println("Welcome to Employee Wage Computation");
+
+		}
+
 	void EmployeeWage(){
-	System.out.println("Welcome to Employee Wage Computation");
-	}
+
+		int isFullTime=1;
+
+		double empCheck= Math.floor(Math.random() * 10) % 2;
+
+		if(empCheck == isFullTime)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is absent");
+
+		}
 
 	public static void main(String args[]) {
-	EmployeeWageBuilder2 employee = new EmployeeWageBuilder2();
-	employee.EmployeeWage();
+		EmployeeWageBuilder2 employee = new EmployeeWageBuilder2();
+		employee.EmployeeWage();
 
-	}
+		}
 }
